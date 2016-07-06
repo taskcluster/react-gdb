92b71c4ad6a79092f9d376dc08466edbdd240ba9
Alexander Baygeldin <a.baygeldin@gmail.com>
2016-07-06 05:01:24 +0300

    middlewares added

---
+++ src/middleware/async.js
@@ -0,0 +1,17 @@
+import { PROMISE } from './promise'
+
+export const ASYNC = Symbol('async')
+
+/**
+ * This middleware is a wrapper around `promiseMiddleware`.
+ * It maps the action with an async function to action with a promise.
+ */
+export let asyncMiddleware = store => next => action => {
+  let type = action.type
+  let inline = action.inline
+  let fn = action[ASYNC]
+  if (!fn) return next(action)
+
+  return { type, inline, [PROMISE]: fn() }
+}
+
---
+++ src/middleware/promise.js
@@ -0,0 +1,31 @@
+import { FAILURE, REQUEST } from '../constants'
+
+export const PROMISE = Symbol('promise')
+
+/**
+ * This middleware handles async actions that has Promises.
+ * It maps the action with a promise to two actions:
+ * the first is REQUEST action (which is just a wrapper around
+ * original action) and the second one is either a FAILURE
+ * action with related error or original action with the results.
+ * Note: by default the object with which promise resolves will
+ * be inlined into action object for convenience. If you want to
+ * access the results of promise execution through `result`
+ * property of the action object, pass `inline: false`.
+ */
+export let promiseMiddleware = store => next => action => {
+  let type = action.type
+  let inline = action.inline !== false
+  let promise = action[PROMISE]
+  if (!promise) return next(action)
+
+  return {
+    type: REQUEST,
+    action: type,
+    promise: promise.then(
+      (result) => inline ? next({ ...result, type }) : next({ result, type }),
+      (error) => next({ error, action: type, type: FAILURE })
+    )
+  }
+}
+
