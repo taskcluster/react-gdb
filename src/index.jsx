e23505286d2292889691e103d9432bfe8277678d
Alexander Baygeldin <a.baygeldin@gmail.com>
2016-07-13 10:31:00 +0300

    Debug tools

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import GDB from 'gdb-js'
-import { createStore, combineReducers,
+import { createStore, combineReducers, compose,
   applyMiddleware, bindActionCreators } from 'redux'
 import { Provider } from 'react-redux'
 import { Map } from 'immutable'
@@ -14,6 +14,8 @@ import initActions from './actions'
 import Sources from './components/sources'
 import Controls from './components/controls'
 
+let isBrowser = typeof window === 'object'
+
 let reducer = combineReducers({
   sources,
   threads,
@@ -21,24 +23,33 @@ let reducer = combineReducers({
   UIState
 })
 
-let createStoreWithMiddleware = applyMiddleware(
+let middlewares = applyMiddleware(
   asyncMiddleware,
   promiseMiddleware
-)(createStore)
+)
 
-let store = createStoreWithMiddleware(reducer)
+let enhancer = compose(
+  middlewares,
+  isBrowser && typeof window.devToolsExtension !== 'undefined' ?
+    window.devToolsExtension() : (f) => f
+)
 
 class ReactGDB extends React.Component {
   constructor (props) {
     super(props)
   }
 
+  componentWillMount () {
+    this.store = createStore(reducer, enhancer)
+  }
+
   componentDidMount () {
     this.gdb = new GDB(this.props.process)
     this.getSource = this.props.sourceProvider
     let actions = initActions(this.gdb, this.getSource)
-    let boundActions = bindActionCreators(actions, store.dispatch)
+    let boundActions = bindActionCreators(actions, this.store.dispatch)
     this.actions = boundActions
+    if (isBrowser) window.reactGDB = boundActions
     this.actions.init()
     this.gdb.on('stopped', this.actions.update)
     this.gdb.on('thread-created', this.actions.addThread)
@@ -46,27 +57,27 @@ class ReactGDB extends React.Component {
   }
 
   render () {
-    let thread = store.UIState.selectedThread
-    let files = store.UIState.get('openedFiles')
+    let thread = this.store.UIState.selectedThread
+    let files = this.store.UIState.get('openedFiles')
     let data = (new Map()).withMutations((map) =>
       files.reduce((res, f) => {
-        let file = store.sources.get(f)
-        let breaks = store.breaks.withMutations((map) => {
+        let file = this.store.sources.get(f)
+        let breaks = this.store.breaks.withMutations((map) => {
           map.filter((b) => b.get('file') === f).toList()
             .filter((b) => b.get('thread') === thread)
             .map((b) => b.get('line'))
         })
         return res.set(f, new Map({ file, breaks }))
       }, map))
-    let frame = store.threads.get(thread).get('frame')
+    let frame = this.store.threads.get(thread).get('frame')
 
     let sourcesList = []
-    store.sources.forEach((value, key) => {
+    this.store.sources.forEach((value, key) => {
       sourcesList.push(<a onClick={() => this.actions.openFile(key)}>key</a><br />)
     })
 
     let threadsList = []
-    store.threads.forEach((value, key) => {
+    this.store.threads.forEach((value, key) => {
       threadsList.push(<a onClick={() => this.actions.selectThread(key)}>key</a><br />)
     })
 
@@ -74,13 +85,13 @@ class ReactGDB extends React.Component {
       this.actions.addBreak(file, pos, thread)
     }
     let removeBreak = (file, pos) => {
-      let id = store.breaks.findKey((value, key) =>
+      let id = this.store.breaks.findKey((value, key) =>
         value.equals({ file, line: pos, thread }))
       this.actions.removeBreak(id)
     }
 
     return (
-      <Provider store={store}>
+      <Provider store={this.store}>
         <div class="reactgdb-wrap">
           <div class="reactgdb-column reactgdb-left">
             <h1>Sources</h1>
@@ -103,11 +114,11 @@ class ReactGDB extends React.Component {
               stepOut={() => this.actions.stepOut(thread)}
               stepIn={() => this.actions.stepIn(thread)} />
             <h1>Context</h1>
-            {store.threads.get(thread).get('vars').toJS()}
+            {this.store.threads.get(thread).get('vars').toJS()}
             <h1>Callstack</h1>
-            {store.threads.get(thread).get('callstack').toJS()}
+            {this.store.threads.get(thread).get('callstack').toJS()}
             <h1>Breakpoints</h1>
-            {store.breaks.filter((b) => b.get('thread') === thread).toJS()}
+            {this.store.breaks.filter((b) => b.get('thread') === thread).toJS()}
           </div>
         </div>
       </Provider>
