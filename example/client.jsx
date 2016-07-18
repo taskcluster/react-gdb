d211dbb3f83f86ac6701bbace1bd365664779ff5
Alexander Baygeldin <a.baygeldin@gmail.com>
2016-07-18 12:50:35 +0300

    fix example

--- example/client.jsx
+++ example/client.jsx
@@ -1,19 +1,32 @@
+/* eslint-env browser */
+
+import React from 'react'
 import { render } from 'react-dom'
 import ReactGDB from '../lib'
 import ExecClient from 'docker-exec-websocket-server/browser'
 
 let client = new ExecClient({
   url: 'ws://localhost:8080/docker-exec',
-  command: ['gdb', '-i=mi', '-tty=/dev/null', 'tickets/main']
+  command: ['gdb', '-i=mi', '-tty=/dev/null', 'tickets/main'],
+  tty: false
 })
 
 async function getFile (filename) {
-  // TODO: Git source provider
+  if (filename.startsWith('/examples')) {
+    let link = 'https://raw.githubusercontent.com/baygeldin/gdb-examples/master' + filename
+    let res = await fetch(link)
+    return await res.text()
+  } else {
+    throw new Error('Not a project file!')
+  }
 }
 
-document.addEventListene('DOMContentLoaded', () => {
-  render(
-    <ReactGDB process={client} sourceProvider={getFile} />,
-    document.getElementById('gdb')
-  )
+document.addEventListener('DOMContentLoaded', () => {
+  // XXX: For reasons unknown, stdin, stdout and stderr are
+  // not set in the constructor, but in the async function.
+  // TODO: Send a PR.
+  client.execute().then(() => {
+    render(<ReactGDB process={client} sourceProvider={getFile} />,
+      document.getElementById('gdb'))
+  })
 })
--- example/server.js
+++ example/server.js
@@ -1,24 +1,27 @@
 import http from 'http'
-import koa from 'koa'
+import Koa from 'koa'
 import serve from 'koa-static'
+import mount from 'koa-mount'
 import { DockerExecServer } from 'docker-exec-websocket-server'
 
 const PORT = 8080
 
-let app = koa()
+let app = new Koa()
+let reactGDB = new Koa()
 
-app.use(serve('./static'))
-app.use(serve('../dist'))
+reactGDB.use(serve('lib/assets'))
+app.use(serve('example/static'))
+app.use(mount('/react-gdb', reactGDB))
 
-
-let server = http.createServer(app.callback()).listen(PORT, () => {
+let server = http.createServer(app.callback())
+server.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`)
 })
 
 let dockerServer = new DockerExecServer({
   path: '/docker-exec',
   server: server,
-  containerId: 'baygeldin/gdb-examples'
+  containerId: 'react-gdb'
 })
 
 export default dockerServer
