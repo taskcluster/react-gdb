2c522f82af1a41652ba93044782185b329673f12
Alexander Baygeldin <a.baygeldin@gmail.com>
2016-08-22 08:42:41 +0300

    refactor actions

--- src/actions.js
+++ src/actions.js
@@ -1,142 +1,175 @@
 import { bindActionCreators } from 'redux'
 import { ASYNC } from './middleware/async.js'
 import { PROMISE } from './middleware/promise.js'
-import { INIT, UPDATE, FETCH, RUN, PROCEED, FOCUS,
-  STEP_IN, STEP_OUT, NEXT, OPEN_FILE, INTERRUPT,
-  CLOSE_FILE, SELECT_THREAD, ADD_THREAD, GET_SOURCES,
+import { INIT, UPDATE_THREAD, FETCH_FILE, RUN, PROCEED, SELECT_POSITION,
+  STEP_IN, STEP_OUT, NEXT, OPEN_FILE, INTERRUPT, APPLY_BREAKS_TO,
+  CLOSE_FILE, SELECT_THREAD, ADD_THREAD, GET_SOURCES, UPDATE_ALL_THREADS,
   REMOVE_THREAD, ADD_BREAK, REMOVE_BREAK } from './constants.js'
 
-export default (gdb, fetchFile, basePath, dispatch) => {
-  let getSources = () => ({
+export default (gdb, sourceProvider, attachOnFork, dispatch, getState) => {
+  let actions = {}
+
+  actions.getSources = () => ({
     type: GET_SOURCES,
-    [ASYNC]: async () => {
-      return (await gdb.sourceFiles())
-        .filter((f) => f.fullname.startsWith(basePath))
-    }
+    [PROMISE]: gdb.sourceFiles({ pattern: sourceProvider.filter })
   })
 
-  let init = () => ({
+  actions.init = () => ({
     type: INIT,
     [ASYNC]: async () => {
       await gdb.init()
       await gdb.enableAsync()
-      dispatch(getSources())
+      if (attachOnFork) await gdb.attachOnFork()
+      dispatch(actions.getSources())
     }
   })
 
-  let exit = () => ({
+  actions.exit = () => ({
     type: EXIT,
     [PROMISE]: gdb.exit()
   })
 
-  let update = (data) => ({
-    type: UPDATE,
+  actions.updateThread = (thread) => ({
+    type: UPDATE_THREAD,
     inline: true,
+    thread,
     [ASYNC]: async () => {
-      // TODO: below should be done by gdb-js
-      let thread = parseInt(data['thread-id'], 10)
-      let callstack = await gdb.callstack(thread)
-      let context = await gdb.context(thread)
-      let filename = data.frame.fullname
-      let line = parseInt(data.frame.line, 10)
-      return { thread, callstack, context, filename, line }
+      let callstack
+      let context
+
+      if (thread.status === 'stopped') {
+        let frame = thread.frame
+
+        try {
+          callstack = await gdb.callstack(thread)
+          context = await gdb.context(thread)
+        } catch (e) {
+          // Probably segmentation fault
+          console.error(e)
+        }
+
+        if (getState().UIState.get('selectedThread') === thread.id) {
+          dispatch(actions.selectPosition(frame.file, frame.line))
+        }
+      }
+
+      return { callstack, context }
     }
   })
 
-  let fetch = (file) => ({
-    type: FETCH,
-    file,
-    [PROMISE]: fetchFile(file)
+  actions.updateAllThreads = () => ({
+    type: UPDATE_ALL_THREADS,
+    [ASYNC]: async () => {
+      let threads = await gdb.threads()
+      console.log(threads)
+      threads.forEach((t) => dispatch(actions.updateThread(t)))
+    }
   })
 
-  let addBreak = (file, pos, thread) => ({
+  actions.fetchFile = (file) => ({
+    type: FETCH_FILE,
+    file,
+    [PROMISE]: sourceProvider.fetch(file)
+  })
+
+  actions.addBreak = (file, pos, thread) => ({
     type: ADD_BREAK,
     [PROMISE]: gdb.addBreak(file, pos, thread)
   })
 
-  let removeBreak = (id) => ({
+  actions.removeBreak = (breakpoint) => ({
     type: REMOVE_BREAK,
-    id,
-    [PROMISE]: gdb.removeBreak(id)
+    breakpoint,
+    [PROMISE]: gdb.removeBreak(breakpoint)
   })
 
-  let run = () => ({
+  actions.run = () => ({
     type: RUN,
     [PROMISE]: gdb.run()
   })
 
-  let proceed = (thread) => ({
+  actions.proceed = (thread) => ({
     type: PROCEED,
     thread,
     [PROMISE]: gdb.proceed(thread)
   })
 
-  let interrupt = (thread) => ({
+  actions.interrupt = (thread) => ({
     type: INTERRUPT,
     [PROMISE]: gdb.interrupt(thread)
   })
 
-  let stepIn = (thread) => ({
+  actions.stepIn = (thread) => ({
     type: STEP_IN,
     [PROMISE]: gdb.stepIn(thread)
   })
 
-  let stepOut = (thread) => ({
+  actions.stepOut = (thread) => ({
     type: STEP_OUT,
     [PROMISE]: gdb.stepOut(thread)
   })
 
-  let next = (thread) => ({
+  actions.next = (thread) => ({
     type: NEXT,
     [PROMISE]: gdb.next(thread)
   })
 
-  let openFile = (file) => ({
-    type: OPEN_FILE,
-    file
-  })
+  actions.openFile = (file) => {
+    if (!getState().sources.get(file)) dispatch(actions.fetchFile(file))
+    return { type: OPEN_FILE, file }
+  }
 
-  let closeFile = (file) => ({
-    type: CLOSE_FILE,
-    file
-  })
-
-  let selectThread = (thread) => ({
-    type: SELECT_THREAD,
-    thread
-  })
-
-  let focus = (file, line) => ({
-    type: FOCUS,
-    file,
-    line
-  })
-
-  let addThread = (data) => ({
-    type: ADD_THREAD,
-    id: data.id,
-    gid: data['group-id']
-  })
-
-  let removeThread = (data) => ({
-    type: REMOVE_THREAD,
-    id: data.id
-  })
-
-  let attachTarget = (pid) => ({
-    type: ATTACH_TARGET,
-    [ASYNC]: async () => {
-      await gdb.attach(pid)
-      dispatch(getSources())
+  actions.closeFile = (file) => {
+    let state = getState().UIState
+    if (state.get('selectedPosition').get('file') === file) {
+      let files = state.get('openedFiles').toArray()
+      let index = files.indexOf(file)
+      dispatch(actions.selectPosition(files[index - 1] || files[index + 1]))
     }
+    return { type: CLOSE_FILE, file }
+  }
+
+  actions.applyBreakpointsTo = (scope) => ({
+    type: APPLY_BREAKS_TO,
+    scope
   })
 
-  return bindActionCreators({
-    init, exit, update, fetch,
-    addBreak, removeBreak, run, proceed,
-    interrupt, stepIn, stepOut, next,
-    openFile, closeFile, selectThread, focus,
-    addThread, removeThread, attachTarget
-  }, dispatch)
+  actions.selectThread = (id) => {
+    let thread = getState().threads.get(id)
+    let frame = thread ? thread.get('thread').frame : null
+    if (frame) dispatch(actions.selectPosition(frame.file, frame.line))
+    return { type: SELECT_THREAD, id }
+  }
+
+  actions.selectPosition = (file, line) => {
+    if (file && !getState().UIState.get('openedFiles').has(file)) {
+      dispatch(actions.openFile(file))
+    }
+    return  { type: SELECT_POSITION, file, line }
+  }
+
+  actions.addThread = (thread) => {
+    if (!getState().UIState.get('selectedThread')) {
+      dispatch(actions.selectThread(thread.id))
+    }
+    return { type: ADD_THREAD, thread }
+  }
+
+  actions.removeThread = (thread) => {
+    let state = getState()
+    if (state.UIState.get('selectedThread').id === thread.id) {
+      let threads = state.threads.keySeq().toArray()
+      let index = threads.indexOf(threads.id)
+      dispatch(actions.selectThread(threads[index - 1] || threads[index + 1]))
+    }
+    return { type: REMOVE_THREAD, thread }
+  }
+
+  actions.attachTarget = (pid) => ({
+    type: ATTACH_TARGET,
+    [PROMISE]: gdb.attach(pid)
+  })
+
+  return bindActionCreators(actions, dispatch)
 }
 
