49b5fce47f9067544ea5b77d241e46c7e8544beb
Alexander Baygeldin <a.baygeldin@gmail.com>
2016-08-22 08:43:33 +0300

    adjust store to build system

--- src/store.js
+++ src/store.js
@@ -2,10 +2,13 @@ import { createStore, combineReducers,
   compose, applyMiddleware } from 'redux'
 import { asyncMiddleware } from './middleware/async.js'
 import { promiseMiddleware } from './middleware/promise.js'
+import { errorMiddleware } from './middleware/error.js'
 import sources from './reducers/sources.js'
 import threads from './reducers/threads.js'
 import breaks from './reducers/breaks.js'
 import UIState from './reducers/ui-state.js'
+
+const DEVELOPMENT = process.env.NODE_ENV === 'development'
 
 let reducer = combineReducers({
   sources,
@@ -14,14 +17,16 @@ let reducer = combineReducers({
   UIState
 })
 
-let middlewares = applyMiddleware(
+let middlewares = [
   asyncMiddleware,
   promiseMiddleware
-)
+]
+
+if (DEVELOPMENT) middlewares.push(errorMiddleware)
 
 let enhancer = compose(
-  middlewares,
-  window && typeof window.devToolsExtension !== 'undefined' ?
+  applyMiddleware.apply(this, middlewares),
+  DEVELOPMENT && window && window.devToolsExtension ?
     window.devToolsExtension() : (f) => f
 )
 
